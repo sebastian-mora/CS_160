3a31fe84f1a3bbe50bd1af1217c58d0eee0f7d26
seb1055 <sebmora1055@gmail.com>
2020-02-20 14:44:26 -0800

    template baseplaste fix

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+const port = 3000
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,11 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -55,5 +52,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
-module.exports = app;
+app.listen(port, () => console.log(`CS-160 app listening on port ${port}!`))
