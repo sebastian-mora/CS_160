a5ccd9cb4cbf8acef67afe1e4b62c48ee757dd10
seb1055 <sebmora1055@gmail.com>
2020-02-20 17:19:42 -0800

    working base

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('views/pages/index.ejs')
+  res.render('pages/index.ejs')
 });
 
 module.exports = router;
