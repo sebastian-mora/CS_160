113f21a59dd36f896660ff05cf067359847b09e6
Jeffrey Persons <jperman8@gmail.com>
2020-02-25 10:48:43 -0800

    Modularize tasks file into endpoints and utilities

--- routes/task.js
+++ routes/task.js
@@ -1,31 +1,52 @@
+// ========= IMPORTS
 const express = require('express');
 const router = express.Router();
 
 
+// ========= UTILITIES
+// lookupTask(uid: int) => json | {} if not found
+function lookupTask(uid) {
+    // todo: replace dummy data with actual lookup result (as json assembled from sql query)
+    if (uid < 0) {
+        return {}
+    }
+    return {
+        'uid': uid,
+        'date_created': '2020-02-24 15:16:30',
+        'date_due':     '2020-02-25 15:16:30',
+        'title':        'my dummy task title',
+        'description':  'my dummy description',
+        'tags':     [],
+        'comments': [],
+        'subtasks': []
+    }
+}
+
+
+// ========= ENDPOINT: FETCHING TASKS
+// todo: perhaps this should return all tasks? or we have filtering parameters
+// or maybe this should be done elsewhere
 router.get('/', function(req, res) {
     res.send("requested task with no id");
 });
 
-// curl -v http://127.0.0.1:3000/task/
+// can test like `curl -v http://127.0.0.1:3000/task/6`
 router.get('/:uid', function(req, res) {
-    console.log("requested task with uid=" + req.params.uid);
-
-    res.send({
-            'uid': '',
-            'date_created': '2020-02-24 15:16:30',
-            'date_due':     '2020-02-25 15:16:30',
-            'title':        'my dummy task title',
-            'description':  'my dummy description',
-            'tags':     [],
-            'comments': [],
-            'subtasks': []
-    });
+    const taskJson = lookupTask(req.params.uid);
+    if (taskJson === {}) {
+        res.status(404).send('Task with uid=' + req.params.uid + "does not exist");
+    } else {
+        res.send(taskJson);
+    }
 });
 
+
+// ========= ENDPOINT: CREATING TASK
 router.use(express.json());
 router.post('/', function(req, res) {
     console.log(request.body);
     response.send(request.body);
 });
 
+// ========= EXPORTS
 module.exports = router;
