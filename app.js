ab16e6d57bb62ebafe2fa67a8e9cc757a7a54970
Jeffrey Persons <jperman8@gmail.com>
2020-02-25 10:29:18 -0800

    Add task endpoint for looking up existing tasks by uid

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var task = require('./routes/task');
 
 var app = express();
 
@@ -20,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/task', task);
+app.use('/task/:uid', task);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', function(req, res) {
+    res.send("requested task with no id");
+});
+
+// curl -v http://127.0.0.1:3000/task/
 router.get('/:uid', function(req, res) {
+    console.log("requested task with uid=" + req.params.uid);
+
     res.send({
+            'uid': '',
             'date_created': '2020-02-24 15:16:30',
             'date_due':     '2020-02-25 15:16:30',
             'title':        'my dummy task title',
