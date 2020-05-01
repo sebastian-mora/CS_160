6e8ff0e1e0c48129bb887df05817517b20789cdf
Sebastian <sebmora1055@gmail.com>
2020-04-30 17:14:52 -0700

    Registe form works but it needs db stuff

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/user', users);
 app.use('/tasks', tasks);
 app.use('/tasks?', tasks);
 app.use('/tasks/:uid', tasks);
