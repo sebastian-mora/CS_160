8adf1956828c2771bbf9f2d62f6c957a30ed966a
Sebastian <sebmora1055@gmail.com>
2020-04-30 17:15:51 -0700

    Register page (#19)
    * Added a register page. Still need hooked up
    
    * Form posts to user/register
    
    * Registe form works but it needs db stuff

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+
+router.post('/register', function(req, res) {
+  // Needs logic
+  console.log("HIT");
+  
+  res.redirect('/login')
+});
+
 module.exports = router;
