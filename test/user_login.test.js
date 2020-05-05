3d14c43ed024d3fbc11b9439a6055d8b19ebec48
Jeffrey Persons <jperman8@gmail.com>
2020-05-04 17:45:13 -0700

    Add login tests

---
+++ test/user_login.test.js
@@ -0,0 +1,37 @@
+const supertest = require('supertest');
+
+let app = require('../app');
+let test_data = require('./test_data');
+
+
+describe('Render Login Page', () => {
+    it('should get registered user from login route for rendering', async () => {
+        const res = await supertest(app).get('/login').send();
+        expect(res.statusCode).toEqual(200);
+    });
+});
+
+describe('Render Register Page', () => {
+    it('should login ', async () => {
+        const res = await supertest(app).get('/login/register').send();
+        expect(res.statusCode).toEqual(200);
+    });
+});
+
+describe('Login to App', () => {
+    it('should redirect non-registered user back to login page', async () => {
+        const res = await supertest(app)
+            .post('/login')
+            .send(test_data.NON_REGISTERED_USER)
+            .expect('Location', '/login');
+        expect(res.statusCode).toEqual(302);
+    });
+    it('should redirect already-registered user to their tasks page and save their id as a cookie', async () => {
+        const res = await supertest(app)
+            .post('/login')
+            .send(test_data.REGISTERED_USER)
+            .expect('Location', '/tasks')
+            .expect('set-cookie').toEqual('userid=' + test_data.REGISTERED_USER.id + ';');
+        expect(res.statusCode).toEqual(302);
+    });
+});
